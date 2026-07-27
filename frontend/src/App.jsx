3f83d3ee3bb95b87c64bcb87d0b28ba8bd8a8877
Akash-bara <akash80920@gmail.com>
2026-07-27 22:54:36 +0530

    Fix LoginPage import case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
+import LogInPage from "./pages/LogInPage";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
 import CartPage from "./pages/CartPage";
@@ -76,7 +76,7 @@ function App() {
 
 						<Route
 							path="/login"
-							element={!user ? <LoginPage /> : <Navigate to="/" />}
+							element={!user ? <LogInPage /> : <Navigate to="/" />}
 						/>
 
 						<Route
--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
 import { useUserStore } from "../stores/useUserStore.js";
 
-const LoginPage = () => {
+const LogInPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
